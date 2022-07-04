ba59916c71ddd120db9c778de431adb22f66f52a
LaserSlime <76707643+LaserSlime@user.noreply.github.com>
2022-07-04 16:47:19 +0200

    Format

--- src/main/java/de/laserslime/antihealthindicator/packetadapters/UpdateHealthAdapter.java
+++ src/main/java/de/laserslime/antihealthindicator/packetadapters/UpdateHealthAdapter.java
@@ -15,8 +15,9 @@ public class UpdateHealthAdapter extends PacketAdapter {
 	@Override
 	public void onPacketSending(PacketEvent event) {
 		float saturation = event.getPacket().getFloat().readSafely(1);
-		// If saturation is 0 the hunger bars will start shaking. To keep this vanilla behaviour we don't rewrite it if it's 0
-		if(saturation > 0)
-			event.getPacket().getFloat().writeSafely(1, Float.NaN); // A value of nan will trick some indicators into not displaying anything
+		// If saturation is 0 the hunger bars will start shaking. To keep this vanilla behaviour we don't
+		// rewrite it if it's 0
+		if(saturation > 0) event.getPacket().getFloat().writeSafely(1, Float.NaN); // A value of nan will trick some indicators into not displaying
+																					// anything
 	}
 }
