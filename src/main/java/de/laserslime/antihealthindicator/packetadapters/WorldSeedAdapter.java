8bdf5437a6b5bac1722144e3f17f6e0bee2c3e1f
LaserSlime <76707643+LaserSlime@users.noreply.github.com>
2024-10-29 22:55:20 +0100

    Better anti seed cracking (stolen from AntiSeedCracker)

--- src/main/java/de/laserslime/antihealthindicator/packetadapters/WorldSeedAdapter.java
+++ src/main/java/de/laserslime/antihealthindicator/packetadapters/WorldSeedAdapter.java
@@ -1,9 +1,13 @@
 package de.laserslime.antihealthindicator.packetadapters;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.bukkit.plugin.Plugin;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.InternalStructure;
 import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 
 public class WorldSeedAdapter extends PacketAdapter {
@@ -14,6 +18,22 @@ public class WorldSeedAdapter extends PacketAdapter {
 
 	@Override
 	public void onPacketSending(PacketEvent event) {
-		event.getPacket().getLongs().writeSafely(0, 0L);
+		PacketContainer packet = event.getPacket();
+		int structureSize = packet.getStructures().size();
+		if(structureSize == 0) {
+			plugin.getLogger().warning("Can not write hashed seed at respawn for player " + event.getPlayer().getName() + ".");
+			return;
+		}
+		InternalStructure structureModifier = packet.getStructures().read(structureSize - 1);
+		structureModifier.getLongs().write(0, randomizeHashedSeed(structureModifier.getLongs().read(0)));
+	}
+
+	public long randomizeHashedSeed(long hashedSeed) {
+		int length = Long.toString(hashedSeed).length();
+		if(length > 18)
+			length = 18;
+		long min = (long) Math.pow(10, length - 1);
+		long max = (long) (Math.pow(10, length) - 1);
+		return ThreadLocalRandom.current().nextLong(min, max + 1);
 	}
 }
