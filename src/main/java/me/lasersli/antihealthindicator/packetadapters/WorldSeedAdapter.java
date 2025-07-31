5ed44269aab1cc4a77ff1c92ea7cc09c7e6eac3a
LaserSlime <76707643+LaserSlime@users.noreply.github.com>
2025-07-31 23:10:45 +0200

    Fix world seed filtering for 1.20.2+ servers

--- src/main/java/me/lasersli/antihealthindicator/packetadapters/WorldSeedAdapter.java
+++ src/main/java/me/lasersli/antihealthindicator/packetadapters/WorldSeedAdapter.java
@@ -1,10 +1,17 @@
 package me.lasersli.antihealthindicator.packetadapters;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
+
 import org.bukkit.plugin.Plugin;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
+
+import me.lasersli.antihealthindicator.util.Version;
 
 public class WorldSeedAdapter extends PacketAdapter {
 
@@ -14,7 +21,35 @@ public class WorldSeedAdapter extends PacketAdapter {
 
 	@Override
 	public void onPacketSending(PacketEvent event) {
-		//TODO this might not work on newer versions because mojang put this data into a seperate class
-		event.getPacket().getLongs().writeSafely(0, 0L);
+		if(Version.getServerVersion().isAtLeast(Version.V1_20_2)) {
+			final int index = event.getPacketType().equals(PacketType.Play.Server.LOGIN) ? 9 : 0;
+			try {
+				Object spawnInfo = event.getPacket().getModifier().read(index);
+				Object[] params = new Object[spawnInfo.getClass().getRecordComponents().length];
+				for(int i = 0; i < params.length; i++) {
+					if(i == 2)
+						params[2] = 0L;
+					else
+						params[i] = spawnInfo.getClass().getRecordComponents()[i].getAccessor().invoke(spawnInfo);
+				}
+				Object newSpawnInfo = getCanonicalConstructor(spawnInfo.getClass()).newInstance(params);
+				event.getPacket().getModifier().write(index, newSpawnInfo);
+			} catch(InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException | NoSuchMethodException ex) {
+				ex.printStackTrace();
+			}
+		} else
+			event.getPacket().getLongs().writeSafely(0, 0L);
+	}
+
+	/**
+	 * From the api note of {@link Class#getRecordComponents()}. Modified so that the parameter doesn't have to be a record.
+	 * 
+	 * @param cls Must be a record
+	 * @return The canonical constructor of the provided record
+	 * @throws NoSuchMethodException If the constructor can't be found
+	 */
+	static Constructor<?> getCanonicalConstructor(Class<?> cls) throws NoSuchMethodException {
+		Class<?>[] paramTypes = Arrays.stream(cls.getRecordComponents()).map(RecordComponent::getType).toArray(Class<?>[]::new);
+		return cls.getDeclaredConstructor(paramTypes);
 	}
 }
